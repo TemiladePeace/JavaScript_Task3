2ab7c0f3d30498bf068fc74abb11dd150fcb968d
peachy01 <temmynladejesu@gmail.com>
2020-04-19 11:22:52 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function replaceNum( input ) {
+    let output = [];
+
+    for( let i = 1; i <= input; i++) {
+        let val = "";
+        if( i % 2 == 0){
+            val = "yu";
+        }
+        if( i % 3 == 0){
+            val = (val == "") ? "gi" : val + "-gi";
+        }
+        if( i % 5 == 0){
+            val = (val == "") ? "oh" : val + "-oh";
+        }
+        if(val == ""){
+            val = i;
+        }
+        output.push(val);
+    }
+    return output;
+}
+console.log(replaceNum(100));
+console.log(replaceNum(30));
